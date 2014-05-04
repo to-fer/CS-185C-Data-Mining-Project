33037e3b43157d929a9d96e816ac5b718ee1e371
to-fer <tony.j.ferrero@gmail.com>
2014-05-03 22:13:32 -0700

    Changed hard coded things into arguments.

--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -6,26 +6,31 @@ import kmeans.SetKMeans
 import java.nio.file.{Paths, Files}
 
 object Main extends App {
-  // Spark initialization
-  val sparkHome = "/home/dash/prog/lang/scala/spark"
-  val master = "local[4]"
-  implicit val context = new SparkContext(master, "Song Set K-Means", sparkHome)
 
-  val transformedDataFile = "transformed-subset"
-  val songSeparator = "<SEP>"
-  // Cached because K-Means is an iterative algorithm
-  val trainingData = context.textFile(transformedDataFile).map(_.split(songSeparator).toSet).cache()
+  if (args.length == 3) {
+    // Spark initialization
+    val sparkHome = args(0)
+    val master = args(1)
+    implicit val context = new SparkContext(master, "Song Set K-Means", sparkHome)
 
-  val (kMeansResults, _) = SetKMeans.run (
-    trainingData = trainingData,
-    k = 10
-  )
+    val transformedDataFile = args(2)
+    val songSeparator = "<SEP>"
+    // Cached because K-Means is an iterative algorithm
+    val trainingData = context.textFile(transformedDataFile).map(_.split(songSeparator).toSet).cache()
 
-  val resultsFile = Paths.get("clustering-results")
-  if (!Files.exists(resultsFile))
-    Files.createFile(resultsFile)
-  Files.write(resultsFile, kMeansResults.toString.getBytes)
+    val (kMeansResults, _) = SetKMeans.run (
+      trainingData = trainingData,
+      k = 10
+    )
 
-  context.stop()
-  System.exit(0) // Make sure everything stops
+    val resultsFile = Paths.get("clustering-results")
+    if (!Files.exists(resultsFile))
+      Files.createFile(resultsFile)
+    Files.write(resultsFile, kMeansResults.toString.getBytes)
+
+    context.stop()
+    System.exit(0) // Make sure everything stops
+  }
+  else
+    println("You must enter the path to a spark installation, master URL, and transformed dataset!")
 }
