e56d9b270e7093ad6232bc8393fa685396e069f2
to-fer <tony.j.ferrero@gmail.com>
2014-05-04 21:06:44 -0700

    Created registrator.

---
+++ src/main/scala/serialization/Registrator.scala
@@ -0,0 +1,10 @@
+package serialization
+
+import com.esotericsoftware.kryo.Kryo
+import org.apache.spark.serializer.KryoRegistrator
+
+class Registrator extends KryoRegistrator {
+  override def registerClasses(kryo: Kryo) = {
+    kryo.register(classOf[Set[String]])
+  }
+}
