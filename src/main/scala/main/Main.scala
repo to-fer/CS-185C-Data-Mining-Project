9e2accc816f0f6eff6c8810eb7443fe72c29c945
to-fer <tony.j.ferrero@gmail.com>
2014-05-04 21:57:31 -0700

    Added a bigger Kryo serialization buffer, and also serialized the training data.

--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -4,6 +4,7 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 import kmeans.SetKMeans
 import java.nio.file.{Paths, Files}
+import org.apache.spark.storage.StorageLevel
 
 object Main extends App {
 
@@ -14,6 +15,7 @@ object Main extends App {
                .setMaster(args(1))
                .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
                .set("spark.kryo.registrator", "serialization.Registrator")
+               .set("spark.kryoserializer.buffer.mb", "10")
                .setAppName("Song Set K-Means")
                .set("spark.executor.memory", "2g")
                .set("spark.cores.max", "4")
@@ -23,7 +25,7 @@ object Main extends App {
     val transformedDataFile = args(2)
     val songSeparator = "<SEP>"
     // Cached because K-Means is an iterative algorithm
-    val trainingData = context.textFile(transformedDataFile).map(_.split(songSeparator).toSet).cache()
+    val trainingData = context.textFile(transformedDataFile).map(_.split(songSeparator).toSet).persist(StorageLevel.MEMORY_ONLY_SER)
 
     val (kMeansResults, _) = SetKMeans.run (
       trainingData = trainingData,
