a5a6b9db23b54f14c3e1ea9e1232c378378197c3
to-fer <tony.j.ferrero@gmail.com>
2014-05-06 11:15:28 -0700

    Playcount feature changes. Now using Kryo serialization.

--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -5,6 +5,7 @@ import org.apache.spark.{SparkConf, SparkContext}
 import kmeans.SetKMeans
 import java.nio.file.{Paths, Files}
 import org.apache.spark.storage.StorageLevel
+import scala.util.parsing.combinator.{JavaTokenParsers, RegexParsers}
 
 object Main extends App {
 
@@ -14,18 +15,35 @@ object Main extends App {
                .setSparkHome(args(0))
                .setMaster(args(1))
                .setAppName("Song Set K-Means")
-               .setJars(List("kmeans.jar"))
                .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
                .set("spark.kryo.registrator", "serialization.Registrator")
                .set("spark.kryoserializer.buffer.mb", "10")
-               .set("spark.executor.memory", "6g")
+               .set("spark.executor.memory", "2g")
 
     implicit val context = new SparkContext(conf)
 
     val transformedDataFile = args(2)
     val songSeparator = "<SEP>"
     // Cached because K-Means is an iterative algorithm
-    val trainingData = context.textFile(transformedDataFile).map(_.split(songSeparator).toSet).persist(StorageLevel.MEMORY_ONLY_SER)
+    val rawTrainingData = context.textFile(transformedDataFile).map(_.split(songSeparator))
+    val trainingData = rawTrainingData.map(splitLine => {
+      val splitLineElems = splitLine.map(lineElem => {
+
+        object SongTupleParser extends RegexParsers {
+          def songTuple: Parser[(String, Int)] = {
+            "(" ~ songNameAndTupSep  ~ playCount ~ ")" ^^
+              { case ("(" ~ sn  ~ pc ~ ")" ) => (sn, pc) }
+          }
+          def songNameAndTupSep: Parser[String] = ".+<TUPSEP>".r ^^
+            { case (songNameWithComma) => songNameWithComma.replaceAll("<TUPSEP>", "") }
+          def playCount: Parser[Int] = "[0-9]+".r ^^ { case (playCountStr) => playCountStr.toInt }
+        }
+        val (songName, playCount) = SongTupleParser.parse(SongTupleParser.songTuple, lineElem).get
+          (songName, playCount.toDouble)
+        })
+
+      splitLineElems.toMap[String, Double]
+    })
 
     val (kMeansResults, _) = SetKMeans.run (
       trainingData = trainingData,
--- src/main/scala/serialization/Registrator.scala
+++ src/main/scala/serialization/Registrator.scala
@@ -5,6 +5,7 @@ import org.apache.spark.serializer.KryoRegistrator
 
 class Registrator extends KryoRegistrator {
   override def registerClasses(kryo: Kryo) = {
-    kryo.register(classOf[Set[String]])
+    kryo.register(classOf[String])
+    kryo.register(classOf[Map[String, Double]])
   }
 }
