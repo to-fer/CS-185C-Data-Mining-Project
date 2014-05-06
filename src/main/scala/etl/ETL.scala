908fcca02992c497feb6be1352118a44e8686404
to-fer <tony.j.ferrero@gmail.com>
2014-05-06 11:10:54 -0700

    Now using SparkConf for configuration and using Kryo serialization. Also added basic argument number validation.

--- src/main/scala/etl/ETL.scala
+++ src/main/scala/etl/ETL.scala
@@ -1,60 +1,74 @@
 package etl
 
 import java.nio.file.{Paths, Files}
-import org.apache.spark.SparkContext
+import org.apache.spark.{SparkConf, SparkContext}
 
 object ETL extends App {
-  val rawDatasetPath = args(2)
-  if (Files.exists(Paths.get(rawDatasetPath))) {
-    val sparkHome = args(0)
-    val appName = "ETL"
-    val masterURL = args(1)
-    val context = new SparkContext(masterURL, appName, sparkHome)
 
-    // For transformation from song ID to song name. File available at
-    // http://labrosa.ee.columbia.edu/millionsong/pages/getting-dataset at "1." under "Additional Files".
-    val translationDataset = context.textFile("unique_tracks.txt")
-    val separator = "<SEP>"
-    val translationDatasetSplitLines =
-      translationDataset.map(_.split(separator))
-    val songIdToSongName = translationDatasetSplitLines.collect {
-      case splitLine: Array[String] if splitLine.length == 4 => {
+  if (args.length == 3) {
+    val rawDatasetPath = args(2)
+    if (Files.exists(Paths.get(rawDatasetPath))) {
+      val sparkConf = new SparkConf()
+        .setSparkHome(args(0))
+        .setAppName("ETL")
+        .setMaster(args(1))
+        .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+        .set("spark.kryo.registrator", "serialization.Registrator")
+        .set("spark.kryoserializer.buffer.mb", "50")
+
+      val context = new SparkContext(sparkConf)
+
+      // For transformation from song ID to song name. File available at
+      // http://labrosa.ee.columbia.edu/millionsong/pages/getting-dataset at "1." under "Additional Files".
+      val translationDataset = context.textFile("unique_tracks.txt")
+      val separator = "<SEP>"
+      val translationDatasetSplitLines =
+        translationDataset.map(_.split(separator))
+      val songIdToSongName = translationDatasetSplitLines.collect {
+        case splitLine: Array[String] if splitLine.length == 4 => {
+          val songId = splitLine(1)
+          val songName = splitLine(3)
+          (songId -> songName)
+        }
+      }.toArray.toMap
+
+
+      /* Transform! */
+      val dataset = context.textFile(rawDatasetPath)
+
+      // Case class, just for readability's sake
+      case class Row(userId: String, songName: String, playCount: Int)
+      val triplets = dataset.map(line => {
+        val splitLine = line.split(",")
         val songId = splitLine(1)
-        val songName = splitLine(3)
-        (songId -> songName)
+        val songName =
+          if (songIdToSongName.contains(songId))
+            songIdToSongName(songId)
+          else
+            songId
+        new Row(splitLine(0), songName, splitLine(2).toInt)
+      })
+      val userSongGrouping = triplets.groupBy(_.userId)
+      val userSongSets = userSongGrouping map {
+        case (userId, userIdRowArray) =>
+          userIdRowArray.map(r => (r.songName, r.playCount)).toSet
       }
-    }.toArray.toMap
 
+      // Write transformed data to a file.
+      val tupleSeparator = "<TUPSEP>"
+      val formattedTupleSets = userSongSets.map(_.map {
+        case (songName, playCount) => s"($songName$tupleSeparator$playCount)"
+      })
+      val fileContents = formattedTupleSets.map(_.mkString(separator))
+      val transformedDataFile = "taste-profile-subset-transformed"
+      fileContents.saveAsTextFile(transformedDataFile)
 
-    /* Transform! */
-    val dataset = context.textFile(rawDatasetPath)
-
-    // Case class, just for readability's sake
-    case class Row(userId: String, songName: String, playCount: Int)
-    val triplets = dataset.map(line => {
-      val splitLine = line.split(",")
-      val songId = splitLine(1)
-      val songName =
-        if (songIdToSongName.contains(songId))
-          songIdToSongName(songId)
-        else
-          songId
-      new Row(splitLine(0), songName, splitLine(2).toInt)
-    })
-    val userSongGrouping = triplets.groupBy(_.userId)
-    val userSongSets = userSongGrouping map {
-      case (userId, userIdRowArray) =>
-        userIdRowArray.map(r => (r.songName, r.playCount)).toSet
+      context.stop()
+      System.exit(0)
     }
-
-    // Write transformed data to a file.
-    val fileContents = userSongSets.map(_.mkString(separator))
-    val transformedDataFile = "taste-profile-subset-transformed"
-    fileContents.saveAsTextFile(transformedDataFile)
-
-    context.stop()
-    System.exit(0)
+    else
+      println(s"The data set to transform ($rawDatasetPath) doesn't exist!")
   }
   else
-    println(s"The data set to transform ($rawDatasetPath) doesn't exist!")
+    println("[spark home] [master URL] [dataset path]")
 }
