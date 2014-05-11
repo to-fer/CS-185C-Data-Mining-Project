6522a537b7682504f3625ecc377499ac4533d601
to-fer <tony.j.ferrero@gmail.com>
2014-05-11 15:29:16 -0700

    Added Kryo registrator.

--- src/main/scala/serialization/Registrator.scala
+++ src/main/scala/serialization/Registrator.scala
@@ -6,5 +6,6 @@ import org.apache.spark.serializer.KryoRegistrator
 class Registrator extends KryoRegistrator {
   override def registerClasses(kryo: Kryo) = {
     kryo.register(classOf[Set[String]])
+    kryo.register(classOf[String])
   }
 }
